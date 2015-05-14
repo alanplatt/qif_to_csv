23e78d7c55eace59ba1212b99469653986bf4e87
Alan Platt <alan.platt@violetblue.co.uk>
2015-05-14 21:54:03 +0100

    rough working script

--- qif_to_csv.py
+++ qif_to_csv.py
@@ -1,13 +1,21 @@
 #
 # Python script to convert qif files to csv
 #
+import re
 
-
+outfile = open("output.csv",'w')
 # open file object and assign to variable
-with open("input.qif") as file:
-  for line in file:
-    print line
+with open("input.qif") as infile:
+  for line in infile:
+    if re.match('^[DT].*', line) is not None:
+      outfile.write(line.rstrip('\r\n')[1:].strip(',') + ",")
+#      print "%s," % line.rstrip('\r\n')[1:],
+#    elif re.match('^T.*', line) is not None:
+#      print "%s," % line.rstrip('\r\n')[1:],
+    elif re.match('^P.*', line) is not None:
+      outfile.write(line.rstrip('\r')[1:].strip(','))
 
+outfile.close()
 
 
 # read file line by line into dictionary
