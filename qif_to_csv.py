39c78381422027eafd0d0f22b3002e6d37c64155
Alan Platt <alan.platt@violetblue.co.uk>
2015-05-14 22:08:20 +0100

    clean up

--- qif_to_csv.py
+++ qif_to_csv.py
@@ -4,19 +4,13 @@
 import re
 
 outfile = open("output.csv",'w')
-# open file object and assign to variable
+
 with open("input.qif") as infile:
   for line in infile:
     if re.match('^[DT].*', line) is not None:
-      outfile.write(line.rstrip('\r\n')[1:].strip(',') + ",")
-#      print "%s," % line.rstrip('\r\n')[1:],
-#    elif re.match('^T.*', line) is not None:
-#      print "%s," % line.rstrip('\r\n')[1:],
+      outfile.write(line.rstrip('\r\n')[1:].strip() + "\t")
     elif re.match('^P.*', line) is not None:
-      outfile.write(line.rstrip('\r')[1:].strip(','))
+      outfile.write(line.rstrip('\r')[1:])
 
 outfile.close()
 
-
-# read file line by line into dictionary
-# print file corretly as csv to new file
