f1d3e6836105f217c8eae88c24c133be48251739
Alan Platt <alan.platt@violetblue.co.uk>
2015-05-14 21:00:13 +0100

    inital commit of script

---
+++ qif_to_csv.py
@@ -0,0 +1,14 @@
+#
+# Python script to convert qif files to csv
+#
+
+
+# open file object and assign to variable
+with open("input.qif") as file:
+  for line in file:
+    print line
+
+
+
+# read file line by line into dictionary
+# print file corretly as csv to new file
